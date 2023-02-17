b099a58e01fac6b188bcb07a96f82c7d526c85de
TravikDev <travikdev@gmail.com>
2023-02-17 15:37:08 +0000

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const fortune = require('./lib/fortune')
 const app = express()
 const port = process.env.PORT || 3000
 const dateServerStarted = performance.now()
 
-const fortunes = [
-    "Победи свои страхи, или они победят тебя",
-    "Рекам нужны истоки",
-    "Не бойся неведомого",
-    "Тебя ждёт приятный сюрприз",
-    "Будь проще везде, где только можно"
-]
 
 app.engine('.hbs', exphbs.engine({
     extname: '.hbs',
@@ -24,8 +18,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', {fortune: randomFortune})
+    
+    res.render('about', {fortune:fortune.getFortune()})
 })
 
 app.use((req, res) => {
